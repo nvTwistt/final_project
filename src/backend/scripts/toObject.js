2d549a141a80b1b57472af6039c9a6fe79e3aedc
nv_twistt <mtamkee@icloud.com>
2021-08-04 11:57:37 -0700

    added symptoms and created custom scripts for db

---
+++ src/backend/scripts/toObject.js
@@ -0,0 +1,447 @@
+const symptoms = 
+[
+  {
+    "ID": 21,
+    "Name": "Itching of skin",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Itch",
+      "Pruritus",
+      "Skin itching"
+    ]
+  },
+  {
+    "ID": 24,
+    "Name": "Blackhead",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      6,
+      23,
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Blackheads",
+      "Comedones"
+    ]
+  },
+  {
+    "ID": 25,
+    "Name": "Skin nodules",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Papula",
+      "Solid elevation of skin",
+      "Thick elevation of skin",
+      "Elevation of skin",
+      "Skin elevation"
+    ]
+  },
+  {
+    "ID": 26,
+    "Name": "Skin lesion",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Pimple",
+      "Small blister",
+      "Pustule",
+      "Skin injury"
+    ]
+  },
+  {
+    "ID": 34,
+    "Name": "Skin wheal",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Hives",
+      "Skin edema",
+      "Skin oedema"
+    ]
+  },
+  {
+    "ID": 61,
+    "Name": "Skin redness",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Erythema",
+      "Red skin",
+      "Red colored skin"
+    ]
+  },
+  {
+    "ID": 62,
+    "Name": "Formation of blisters on a skin area",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      6,
+      25,
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Vesicle",
+      "Formation of blisters on a place of the skin",
+      "Formation of blisters on an area of the skin",
+      "Formation of blisters on an area of skin",
+      "Blisters"
+    ]
+  },
+  {
+    "ID": 63,
+    "Name": "Non-healing skin wound",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": []
+  },
+  {
+    "ID": 65,
+    "Name": "Irregular mole",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": []
+  },
+  {
+    "ID": 105,
+    "Name": "Yellow colored skin",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Jaundice",
+      "Icterus",
+      "Yellowish discoloration of the skin",
+      "Yellowish skin",
+      "Yellow skin"
+    ]
+  },
+  {
+    "ID": 124,
+    "Name": "Skin rash",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": []
+  },
+  {
+    "ID": 134,
+    "Name": "Crusting",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Encrustation",
+      "Crust formation"
+    ]
+  },
+  {
+    "ID": 138,
+    "Name": "Sweating",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      47,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Perspiration",
+      "Diaphoresis",
+      "Excessive sweating",
+      "Sweating to unusual degree"
+    ]
+  },
+  {
+    "ID": 139,
+    "Name": "Cold sweats",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      47,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": []
+  },
+  {
+    "ID": 149,
+    "Name": "Hot flushes",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46,
+      47
+    ],
+    "ProfName": "",
+    "Synonyms": []
+  },
+  {
+    "ID": 150,
+    "Name": "Pallor",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Paleness"
+    ]
+  },
+  {
+    "ID": 151,
+    "Name": "Dry skin",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": []
+  },
+  {
+    "ID": 177,
+    "Name": "Muscle pain",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      7,
+      48,
+      10,
+      49,
+      17,
+      47,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Myalgia"
+    ]
+  },
+  {
+    "ID": 184,
+    "Name": "Hardening of the skin",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Hardening of skin",
+      "Induration"
+    ]
+  },
+  {
+    "ID": 187,
+    "Name": "Wound",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": []
+  },
+  {
+    "ID": 214,
+    "Name": "Flaking skin",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Scaling of skin",
+      "Flaking",
+      "Desquamation"
+    ]
+  },
+  {
+    "ID": 215,
+    "Name": "Moist and softened skin",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Softened skin"
+    ]
+  },
+  {
+    "ID": 217,
+    "Name": "Skin thickening",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Hyperkeratosis",
+      "Thickening of skin"
+    ]
+  },
+  {
+    "ID": 218,
+    "Name": "Coarsening of the skin structure",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": []
+  },
+  {
+    "ID": 220,
+    "Name": "Increased touch sensitivity",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": []
+  },
+  {
+    "ID": 240,
+    "Name": "Blue spot on skin",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Hematoma"
+    ]
+  },
+  {
+    "ID": 987,
+    "Name": "Muscle weakness",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      7,
+      48,
+      10,
+      49,
+      17,
+      47,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Muscular weakness",
+      "Myasthenia"
+    ]
+  },
+  {
+    "ID": 991,
+    "Name": "Blue colored skin",
+    "HasRedFlag": true,
+    "HealthSymptomLocationIDs": [
+      6,
+      25,
+      7,
+      30,
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Blue colored lips",
+      "Blue colored nails",
+      "Blue colored fingertips",
+      "Cyanosis",
+      "Bluish discoloration of the skin",
+      "Bluish skin",
+      "Blue skin"
+    ]
+  },
+  {
+    "ID": 1001,
+    "Name": "Scar",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": []
+  },
+  {
+    "ID": 1004,
+    "Name": "Night sweats",
+    "HasRedFlag": false,
+    "HealthSymptomLocationIDs": [
+      17,
+      47,
+      46
+    ],
+    "ProfName": "",
+    "Synonyms": [
+      "Night sweat",
+      "Nocturnal hyperhidrosis",
+      "Sleep hyperhidrosis",
+      "Sweating at night"
+    ]
+  }
+]
+////////
+//object format
+////////
+result = {}
+for (let i of symptoms) {
+  let id = i.ID;
+  let symptom = i.Name;
+  let flag = i.HasRedFlag;
+  //console.log(id, symptom, flag)
+  result[symptom] = [id, flag]
+}
+console.log(result);
