9d4da728edece8b381eeec935e7810d8dce0ab3f
Cam <cammacdougall12@gmail.com>
2021-08-05 09:57:07 -0600

    Navigation bar changes based on if user is logged in

--- src/components/navigationBar/NavigationBar.js
+++ src/components/navigationBar/NavigationBar.js
@@ -1,23 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './NavigationBar.css'
+import "./NavigationBar.css";
+import { useContext } from "react";
+import { authContext } from "../login/AuthProvider";
 
 export default function NavigationBar(props) {
+  const { auth, user } = useContext(authContext);
   return (
     <nav>
-       <Link to={'/'}>Diagnose.It</Link>
+      <Link to={"/"}>Diagnose.It</Link>
       <ul>
+        {auth && <li>{`Logged in as ${user.name}`}</li>}
+        {!auth && (
+          <li>
+            <Link to={"/patients/login"}>Patient Login</Link>{" "}
+          </li>
+        )}
+
+        {!auth && (
+          <li>
+            <Link to={"/doctors/login"}>Doctor Login</Link>
+          </li>
+        )}
         <li>
-          <Link to={'/patients/login'}>Patient Login</Link>
+          <Link to={"/register"}>Register</Link>
         </li>
         <li>
-          <Link to={'/doctors/login'}>Doctor Login</Link>
-        </li>
-        <li>
-          <Link to={'/register'}>Register</Link>
-        </li>
-        <li>
-          <Link to={'/appointments'}>Appointments</Link>
+          <Link to={"/appointments"}>Appointments</Link>
         </li>
       </ul>
     </nav>
