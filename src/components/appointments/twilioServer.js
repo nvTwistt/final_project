e2048a8d65e45ae12bd794e52a50a30e6de070eb
mtamkee <33907017+mtamkee@users.noreply.github.com>
2021-08-10 10:26:30 -0600

    added punctuation

--- src/components/appointments/twilioServer.js
+++ src/components/appointments/twilioServer.js
@@ -39,7 +39,7 @@ app.post('/message', function (req, res) {
   let diagnosis = payload['diagnosis'];
   let introduction = `Hello ${information.name}.`
   let bodyMessaage = ` Your results show that there is a ${diagnosis.accuracy}% chance that you have a ${diagnosis.symptom}.`
-  let reccomendation = ` Based off the diagnosis, we are reccomending you to see a doctor who specializes in ${diagnosis.specialization_1} or ${diagnosis.specialization_2}`
+  let reccomendation = ` Based off the diagnosis, we are reccomending you to see a doctor who specializes in ${diagnosis.specialization_1} or ${diagnosis.specialization_2}.`
   let completeMessage = introduction + bodyMessaage + reccomendation;
   let outboudNumber = numberCleaner.format(information.to);
   let doctorsMessage = `You have received an appointment request from ${information.name}. They are experiencing a ${diagnosis.symptom}.`
