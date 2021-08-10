24f412018c1865ed0025ef9b58f82a07b1cb82f5
cmacdougall12 <cammacdougall12@gmail.com>
2021-08-10 15:16:03 -0600

    reformatted navbar with boostrap

--- src/components/navigationBar/NavigationBar.js
+++ src/components/navigationBar/NavigationBar.js
@@ -4,39 +4,30 @@ import "./NavigationBar.css";
 import Logout from "../login/Logout";
 import { useContext } from "react";
 import { authContext } from "../login/AuthProvider";
+import { Navbar, Nav, Container, Alert } from "react-bootstrap";
 
 export default function NavigationBar(props) {
   const { auth, user } = useContext(authContext);
   return (
-    <nav>
-      <Link to={"/"}>Diagnose.It</Link>
-      <ul>
-        {auth && <li>{`Logged in as ${user.name}`}</li>}
-        {auth && (
-          <li>
-            <Link to={"/appointmets"}>My Appointments</Link>{" "}
-          </li>
-        )}
-        {auth && <li><Logout/></li>}
+    <Navbar className="navbar-custom" variant="dark">
+      <Navbar.Brand href="/">
+        Diagnose.It
+      </Navbar.Brand>
 
-        {!auth && (
-          <li>
-            <Link to={"/patients/login"}>Patient Login</Link>{" "}
-          </li>
-        )}
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ms-auto">
+        {auth && <Nav.Link className="user-logged-in">{`Logged in as ${user.name}`}</Nav.Link>}
+        {auth && <Nav.Link href="/appointmets">My Appointments</Nav.Link>}
+        {auth && <Nav.Item><Logout /></Nav.Item>}
 
-        {!auth && (
-          <li>
-            <Link to={"/doctors/login"}>Doctor Login</Link>
-          </li>
-        )}
+        {!auth && <Nav.Link href="/patients/login">Patient Login</Nav.Link>}
 
-        {!auth && (
-          <li>
-            <Link to={"/register"}>Register</Link>
-          </li>
-        )}
-      </ul>
-    </nav>
+        {!auth && <Nav.Link href="/doctors/login">Doctor Login</Nav.Link>}
+
+        {!auth && <Nav.Link href="/register">Register</Nav.Link>}
+      </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
